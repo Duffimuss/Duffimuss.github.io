67cd2155074a5411b521b6d43092e199e03a1a0a
Duffimuss <35618889+Duffimuss@users.noreply.github.com>
2022-07-16 14:34:22 -0400

    start of header component - added resume download and intro

--- my-portfolio/src/components/header/Header.jsx
+++ my-portfolio/src/components/header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Chris Gunter</h1>
+        <h5 className="text-light">Software Engineer</h5>
+        <CTA />
+
+        
+      </div>
+    </header>
   )
 }
 
