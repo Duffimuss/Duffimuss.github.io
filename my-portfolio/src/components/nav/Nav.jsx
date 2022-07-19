8656ed5b872f6da5987435c8cd1f0f76ef960a5a
Duffimuss <35618889+Duffimuss@users.noreply.github.com>
2022-07-18 20:12:59 -0400

    finished about page and stylized about page

--- my-portfolio/src/components/nav/Nav.jsx
+++ my-portfolio/src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {BiUserCircle} from 'react-icons/bi'
 import {BiCodeAlt} from 'react-icons/bi'
 import {BiBriefcaseAlt2} from 'react-icons/bi'
 import {BiChat} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><BiHome /></a>
-      <a href="#about"><BiUserCircle /></a>
-      <a href="#experience"><BiCodeAlt /></a>
-      <a href="#services"><BiBriefcaseAlt2 /></a>
-      <a href="#contact"><BiChat /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiCodeAlt /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BiBriefcaseAlt2 /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiChat /></a>
     </nav>
   )
 }
