fce5e4abee5f058f9baf00c4c844d11704b3a83d
Duffimuss <35618889+Duffimuss@users.noreply.github.com>
2022-07-18 20:49:07 -0400

    finished experience, stylized experience, finished stylizing about css

--- my-portfolio/src/components/experience/Experience.jsx
+++ my-portfolio/src/components/experience/Experience.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Java</h4>
+              <small className='text-light'>Experienced  (5+ years)</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Python</h4>
+              <small className='text-light'>Intermediate  (1+ years)</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className='text-light'>Intermediate  (1+ years)</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced  (3+ years)</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate  (2+ years)</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>SQL</h4>
+              <small className='text-light'>Intermediate (2+ years)</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>PostgreSql</h4>
+              <small className='text-light'>Intermediate (1+ years)</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>NodeJS</h4>
+              <small className='text-light'>Novice (~ 1 year)</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>MySQL</h4>
+              <small className='text-light'>Intermediate (2+ years)</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Python</h4>
+              <small className='text-light'>Beginner (1+ year)</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
