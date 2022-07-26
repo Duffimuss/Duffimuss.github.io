669b0d4f5bb8a6a105f3e9eb4beba4a57e951f0c
Duffimuss <35618889+Duffimuss@users.noreply.github.com>
2022-07-26 18:12:10 -0400

    final touches

--- my-portfolio/src/components/nav/Nav.jsx
+++ my-portfolio/src/components/nav/Nav.jsx
@@ -5,6 +5,7 @@ import {BiUserCircle} from 'react-icons/bi'
 import {BiCodeAlt} from 'react-icons/bi'
 import {BiBriefcaseAlt2} from 'react-icons/bi'
 import {BiChat} from 'react-icons/bi'
+import {BiMailSend} from 'react-icons/bi'
 import {useState} from 'react'
 
 const Nav = () => {
@@ -15,7 +16,8 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiCodeAlt /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BiBriefcaseAlt2 /></a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiChat /></a>
+      <a href="#chat" onClick={() => setActiveNav('#chat')} className={activeNav === '#chat' ? 'active' : ''}><BiChat /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMailSend /></a>
     </nav>
   )
 }
